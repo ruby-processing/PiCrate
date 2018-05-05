ed3b5fa4985e490e0bdc751caef3d5c76a8414a5
monkstone <mamba2928@yahoo.co.uk>
2018-05-05 07:59:29 +0100

    processing-core

---
+++ pom.rb
@@ -0,0 +1,108 @@
+project 'picrate', 'http://maven.apache.org' do
+
+  model_version '4.0.0'
+  id 'ruby-processing:picrate:0.0.1'
+  packaging 'jar'
+
+  description 'An integrated processing-core (somewhat hacked), with additional java code for a jruby version of processing.'
+
+  organization 'ruby-processing', 'https://ruby-processing.github.io'
+
+  {
+    'monkstone' => 'Martin Prout', 'benfry' => 'Ben Fry',
+    'REAS' => 'Casey Reas', 'codeanticode' => 'Andres Colubri'
+  }.each do |key, value|
+    developer key do
+      name value
+      roles 'developer'
+    end
+  end
+  license 'GPL 3', 'http://www.gnu.org/licenses/gpl-3.0-standalone.html'
+  license 'LGPL 2', 'https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html'
+
+  issue_management 'https://github.com/ruby-processing/PiCrate/issues', 'Github'
+
+  properties( 'jogl.version' => '2.3.2',
+    'jruby.api' => 'http://jruby.org/apidocs/',
+    'source.directory' => 'src',
+    'maven.compiler.target' => '1.8',
+    'processing.api' => 'http://processing.github.io/processing-javadocs/core/',
+    'picrate.basedir' => '${project.basedir}',
+    'project.build.sourceEncoding' => 'utf-8',
+    'polyglot.dump.pom' => 'pom.xml',
+    'maven.compiler.source' => '1.8' )
+    pom 'org.jruby:jruby:9.1.16.0'
+    jar 'org.processing:video:3.0.2'
+    jar 'org.jogamp.jogl:jogl-all:${jogl.version}'
+    jar 'org.jogamp.gluegen:gluegen-rt-main:${jogl.version}'
+
+    overrides do
+      plugin :resources, '2.6'
+      plugin :dependency, '2.10' do
+        execute_goals( id: 'default-cli',
+          artifactItems: [ { groupId:  'org.jogamp.jogl',
+            artifactId:  'jogl-all',
+            version:  '${jogl.version}',
+            type:  'jar',
+            outputDirectory: '${picrate.basedir}/lib'
+          },
+          { groupId:  'org.jogamp.gluegen',
+            artifactId:  'gluegen-rt',
+            version:  '${jogl.version}',
+            type:  'jar',
+            outputDirectory: '${picrate.basedir}/lib'
+          },
+          { groupId:  'org.jogamp.jogl',
+            artifactId:  'jogl-all',
+            version:  '${jogl.version}',
+            classifier: 'natives-linux-amd64',
+            type:  'jar',
+            outputDirectory: '${picrate.basedir}/lib'
+          },
+          { groupId:  'org.jogamp.gluegen',
+            artifactId:  'gluegen-rt',
+            version:  '${jogl.version}',
+            type:  'jar',
+            classifier: 'natives-linux-amd64',
+            outputDirectory: '${picrate.basedir}/lib'
+          },
+          { groupId:  'org.jogamp.jogl',
+            artifactId:  'jogl-all',
+            version:  '${jogl.version}',
+            classifier: 'natives-macosx-universal',
+            type:  'jar',
+            outputDirectory: '${picrate.basedir}/lib'
+          },
+          { groupId:  'org.jogamp.gluegen',
+            artifactId:  'gluegen-rt',
+            version:  '${jogl.version}',
+            type:  'jar',
+            classifier: 'natives-macosx-universal',
+            outputDirectory: '${picrate.basedir}/lib'
+          }
+        ]
+      )
+    end
+  end
+
+  plugin( :resources, '2.7',
+    'encoding' =>  'UTF-8' )
+    plugin( :compiler, '3.7.0',
+      'source' =>  '1.8',
+      'target' =>  '1.8' )
+      plugin( :pmd, '3.3',
+        'sourceEncoding' =>  'utf-8',
+        'minimumTokens' =>  '100',
+        'targetJdk' =>  '${compileSource}' )
+        build do
+          resource do
+            directory '${source.directory}/main/java'
+            includes ['**/**/*.glsl', '**/*.jnilib']
+            excludes '**/**/*.java'
+          end
+          resource do
+            directory '${source.directory}/main/resources'
+            includes ['**/*.png', '*.txt']
+          end
+        end
+      end
---
+++ src/main/java/processing/javafx/PGraphicsFX2D.java
@@ -0,0 +1,354 @@
+package processing.javafx;
+
+import processing.core.PGraphics;
+import processing.core.PImage;
+import processing.core.PMatrix;
+import processing.core.PMatrix2D;
+import processing.core.PMatrix3D;
+import processing.core.PShape;
+import processing.core.PSurface;
+
+public class PGraphicsFX2D extends PGraphics{
+    final String message = "FX2D renderer not supported in this version of propane";
+    public PGraphicsFX2D(){
+    }
+
+    
+    public void applyMatrix(float n00, float n01, float n02, float n10, float n11, float n12) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void applyMatrix(float n00, float n01, float n02, float n03, float n10, float n11, float n12, float n13, float n20, float n21, float n22, float n23, float n30, float n31, float n32, float n33) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    protected void backgroundImpl(){
+        throw new UnsupportedOperationException(message); 
+    }
+    
+    public void beginContour() {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void beginDraw() {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void beginShape(int kind) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void bezierDetail(int detail) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void bezierVertex(float x1, float y1, float x2, float y2, float x3, float y3) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void bezierVertex(float x2, float y2, float z2, float x3, float y3, float z3, float x4, float y4, float z4) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void box(float w, float h, float d) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public PSurface createSurface() {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void curveDetail(int detail) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void curveVertex(float x, float y, float z) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void endContour() {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void endDraw() {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void endShape(int mode) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void flush() {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public int get(int x, int y) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public PMatrix getMatrix() {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public PMatrix2D getMatrix(PMatrix2D target) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public PMatrix3D getMatrix(PMatrix3D target) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public Object getNative() {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void line(float x1, float y1, float x2, float y2) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void loadPixels() {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public PShape loadShape(String filename) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public PShape loadShape(String filename, String options) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void mask(PImage alpha) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void noClip() {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void point(float x, float y) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void popMatrix() {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void printMatrix() {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void pushMatrix() {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void quad(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void quadraticVertex(float ctrlX, float ctrlY, float endX, float endY) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void quadraticVertex(float x2, float y2, float z2, float x4, float y4, float z4) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void resetMatrix() {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void rotate(float angle) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void rotate(float angle, float vx, float vy, float vz) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void rotateX(float angle) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void rotateY(float angle) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void rotateZ(float angle) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void scale(float s) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void scale(float sx, float sy) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void scale(float sx, float sy, float sz) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public float screenX(float x, float y) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public float screenX(float x, float y, float z) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public float screenY(float x, float y) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public float screenY(float x, float y, float z) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public float screenZ(float x, float y, float z) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void set(int x, int y, int argb) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void setMatrix(PMatrix2D source) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void setMatrix(PMatrix3D source) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void shearX(float angle) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void shearY(float angle) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void sphere(float r) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void strokeCap(int cap) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void strokeJoin(int join) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void strokeWeight(float weight) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public float textAscent() {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public float textDescent() {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void texture(PImage image) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void translate(float tx, float ty) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void triangle(float x1, float y1, float x2, float y2, float x3, float y3) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void vertex(float x, float y) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void vertex(float x, float y, float z) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void vertex(float[] v) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void vertex(float x, float y, float u, float v) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+    
+    public void vertex(float x, float y, float z, float u, float v) {
+        throw new UnsupportedOperationException(message); 
+    }
+
+}
