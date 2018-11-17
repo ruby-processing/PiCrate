f537c0990b0a219dcda952472c1c236e87940195
Martin Prout <mamba2928@yahoo.co.uk>
2018-11-17 07:30:38 +0000

    geany cofiguration

---
+++ vendors/picrate.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env jruby
+# Sketch class
+class MySketch < Processing::App
+  def settings
+    size 200, 200
+  end
+
+  def setup
+    background 0
+  end
+
+  def draw
+    # animation loop
+  end
+end
+
+MySketch.new
