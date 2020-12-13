fe5c0256f5f72178b08e02c6727cbbeee92cdce8
Martin Prout <mamba2928@yahoo.co.uk>
2020-12-13 15:19:55 +0000

    fix numeric

--- lib/picrate.rb
+++ lib/picrate.rb
@@ -9,4 +9,5 @@ end
 Dir["#{PICRATE_ROOT}/lib/*.jar"].sort.each do |jar|
   require jar
 end
-require_relative 'picrate/app'
+require "#{PICRATE_ROOT}/lib/picrate/app"
+require "#{PICRATE_ROOT}/lib/picrate/helpers/numeric"
