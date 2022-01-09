59223058cefb00c9e4d5505332c68c6b1ceef710
monkstone <mamba2928@yahoo.co.uk>
2022-01-09 09:37:54 +0000

    Refactor GfxRender

--- lib/picrate/app.rb
+++ lib/picrate/app.rb
@@ -12,8 +12,8 @@ module Processing
   # JRuby::Util.load_ext('monkstone.PicrateLibrary')
   # A utility to facilitate rendering of Vec2D and Vec3D as vertex
   module Render
-    java_import 'monkstone.vecmath.GfxRender'
-    java_import 'monkstone.vecmath.ShapeRender'
+    import 'monkstone.vecmath.GfxRender'
+    import 'monkstone.vecmath.ShapeRender'
   end
 
   # This class is the base class the user should inherit from when making
